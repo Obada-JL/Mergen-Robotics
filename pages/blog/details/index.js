a25cb59bc5ad216c6361ec0ecbc30351886ff506
Obada-JL <leader.obada@gmail.com>
2024-05-23 17:29:17 +0300

    hhh

--- pages/blog/details/index.js
+++ pages/blog/details/index.js
@@ -1,27 +1,27 @@
-import React from "react";
-import NavbarTwo from "../../../components/Layouts/NavbarTwo";
-import PageBanner from "../../../components/Common/PageBanner";
-import BlogDetailsContent from "../../../components/Blog/BlogDetailsContent";
-import Footer from "../../../components/Layouts/Footer";
+// import React from "react";
+// import NavbarTwo from "../../../components/Layouts/NavbarTwo";
+// import PageBanner from "../../../components/Common/PageBanner";
+// import BlogDetailsContent from "../../../components/Blog/BlogDetailsContent";
+// import Footer from "../../../components/Layouts/Footer";
 
-const BlogDetails = () => {
-  return (
-    <>
-      <NavbarTwo />
+// const BlogDetails = () => {
+//   return (
+//     <>
+//       <NavbarTwo />
 
-      <PageBanner
-        pageTitle="Blog Details"
-        homePageUrl="/"
-        homePageText="Home"
-        activePageText="Blog Details"
-        bgImg="/images/page-title-bg3.jpg"
-      />
+//       <PageBanner
+//         pageTitle="Blog Details"
+//         homePageUrl="/"
+//         homePageText="Home"
+//         activePageText="Blog Details"
+//         bgImg="/images/page-title-bg3.jpg"
+//       />
 
-      <BlogDetailsContent />
+//       <BlogDetailsContent />
 
-      <Footer />
-    </>
-  );
-};
+//       <Footer />
+//     </>
+//   );
+// };
 
-export default BlogDetails;
+// export default BlogDetails;
